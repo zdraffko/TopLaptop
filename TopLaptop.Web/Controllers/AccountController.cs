255ab4346134045242c6a0867069b1248d1ef56b
zdraffko <zdravkomb1@gmail.com>
2020-01-10 22:54:50 +0200

    Implemented Log In and Log Out.

--- TopLaptop.Web/Controllers/AccountController.cs
+++ TopLaptop.Web/Controllers/AccountController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using TopLaptop.Data.Identity;
 using TopLaptop.Web.ViewModels;
@@ -52,11 +54,51 @@ namespace TopLaptop.Web.Controllers
 
                 foreach (var error in identityResult.Errors)
                 {
-                    ModelState.AddModelError("", error.Description);
+                    ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
 
             return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(LogInViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var signInResult = await signInManager.PasswordSignInAsync(
+                        viewModel.Username,
+                        viewModel.Password,
+                        viewModel.IsRemembered,
+                        false
+                    );
+
+                if (signInResult.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("failedLogInAttemt", "Invalid username or password");
+            }
+
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            if (HttpContext.Request.Method == HttpMethod.Post.Method)
+            {
+                await signInManager.SignOutAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
         }
     }
 }
---
+++ TopLaptop.Web/ViewModels/LogInViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopLaptop.Web.ViewModels
+{
+    public class LogInViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Account Type")]
+        public string AccountType { get; set; }
+
+        [Required]
+        [Display(Name = "Remember me")]
+        public bool IsRemembered { get; set; }
+    }
+}
