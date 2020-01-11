ec0320dc53b52b8bf1181af98821c9d56a840a50
zdraffko <zdravkomb1@gmail.com>
2020-01-11 22:06:13 +0200

    Implemented CRUD for storage.

---
+++ TopLaptop.Web/Controllers/StoragesController.cs
@@ -0,0 +1,138 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TopLaptop.Data.Context;
+using TopLaptop.Data.Entities.Laptops.LaptopParts;
+
+namespace TopLaptop.Web.Controllers
+{
+    public class StoragesController : Controller
+    {
+        private readonly TopLaptopDbContext _context;
+
+        public StoragesController(TopLaptopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Storages.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var storage = await _context.Storages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (storage == null)
+            {
+                return NotFound();
+            }
+
+            return View(storage);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Type,Size")] Storage storage)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(storage);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(storage);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var storage = await _context.Storages.FindAsync(id);
+            if (storage == null)
+            {
+                return NotFound();
+            }
+            return View(storage);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type,Size")] Storage storage)
+        {
+            if (id != storage.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(storage);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StorageExists(storage.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(storage);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var storage = await _context.Storages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (storage == null)
+            {
+                return NotFound();
+            }
+
+            return View(storage);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var storage = await _context.Storages.FindAsync(id);
+            _context.Storages.Remove(storage);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StorageExists(int id)
+        {
+            return _context.Storages.Any(e => e.Id == id);
+        }
+    }
+}
